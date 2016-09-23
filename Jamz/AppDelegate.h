de17345d0f3c0d16a2c1d227006a380ad1fbeb9f
shauntc <shaun@shauntc.com>
2016-09-23 10:16:27 -0700

    Initial Commit

---
+++ Jamz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jamz
+//
+//  Created by Shaun Campbell on 2016-09-23.
+//  Copyright © 2016 Shaun Campbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jamz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jamz
+//
+//  Created by Shaun Campbell on 2016-09-23.
+//  Copyright © 2016 Shaun Campbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
